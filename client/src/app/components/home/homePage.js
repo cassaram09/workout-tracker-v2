c3c9c823424f8596789d4e09365f18e0d4c4e589
cassaram09 <cassaram09@gmail.com>
2017-12-06 22:15:25 -0500

    add more scss files; testing homePage component and sass compiling

---
+++ client/src/app/components/home/homePage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class HomePage extends Component {
+
+  render(){
+    return (
+      <div className='home'>
+        <h1 className='home__title'>Home.</h1>
+      </div>
+    )
+   
+  }
+
+}
+
+export default HomePage;
